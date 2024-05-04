1769ba0ff6644a608226f0937aa9aefb5f4f1524
emilylasecki <168912366+emilylasecki@users.noreply.github.com>
2024-05-04 09:25:45 -0400

    BadAI.java
    Plays a random move to the tic-tac-toe frame.

---
+++ BadAI.java
@@ -0,0 +1,13 @@
+package TTTbroke;
+
+public class BadAI extends AI {
+    public BadAI(String[] plays, boolean xturn) {
+    }
+    public static void playMove(String[] plays, boolean xturn) { 
+      
+        playSimpleMove(plays, xturn); 
+        } 
+    
+    
+}
+
